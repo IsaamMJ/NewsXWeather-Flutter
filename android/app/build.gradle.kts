4cf526484ec4889ee86739f62110f3296dd50f6d
IsaamMJ <136082478+IsaamMJ@users.noreply.github.com>
2025-09-14 14:19:08 +0530

    release update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.pearl.skyfeed"
         minSdk = 30
         targetSdk = flutter.targetSdkVersion
-        versionCode = 11
-        versionName = "07092025-2"
+        versionCode = 12
+        versionName = "14092025-3"
     }
 
     signingConfigs {
